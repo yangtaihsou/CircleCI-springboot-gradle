1155b73df3a2713df97ea475cca9716a95b7eb84
Kuan Yang <yangkuan@yeah.net>
2020-07-28 17:12:47 +0800

    test

---
+++ src/test/java/com/example/circleci/RebaseTest.java
@@ -0,0 +1,12 @@
+package com.example.circleci;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+@Slf4j
+public class RebaseTest {
+  @Test
+  public void test001(){
+    log.info("test001-------");
+  }
+
+}
